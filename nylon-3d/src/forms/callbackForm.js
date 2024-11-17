4aa6b3c6119c4c8728079eb55fab2e1d2c968e5c
N.Zhilcov <d.chertkov@getproduction.ru>
2024-11-18 00:51:28 +0500

    update

--- nylon-3d/src/forms/callbackForm.js
+++ nylon-3d/src/forms/callbackForm.js
@@ -1,12 +1,16 @@
-import './callbackFirm.css';
+import styles from './callbackForm.module.css';
 
 
 
 function CallbackForm() {
     return (
-      <div>
-
-      </div>
+      <form className={styles.callbackForm}>
+        <div style={{color:'#fff'}}>Вы можете написать нам и мы обязательно вам перезвоним!</div>
+        <input className={styles.formInput} type="text" placeholder="Ф.И.О"></input>
+        <input className={styles.formInput} type="text" placeholder="Телефон"></input>
+        {/* <input className={styles.formInput} type="file"></input> */}
+        <button className={styles.buttonInput} type='submit'>Связаться с нами</button>
+      </form>
     );
   }
   
--- nylon-3d/src/sections/mainsection.js
+++ nylon-3d/src/sections/mainsection.js
@@ -1,5 +1,6 @@
 import './mainsection.css';
 import mainBack from './img/main.png'
+import CallbackForm from '../forms/callbackForm';
 
 
 function Mainsection() {
@@ -7,10 +8,10 @@ function Mainsection() {
             <section className='section'>    
                     <div className='container mainContainer' style={{ backgroundImage: `url(${mainBack})`}}>
                         <div className='mainSlider'>
-
                         </div>
                         <div className='mainTitle'>
                             <h1>Студия 3D печати<br/>NYLON</h1>
+                            <CallbackForm />
                         </div>
                     </div>
                     
