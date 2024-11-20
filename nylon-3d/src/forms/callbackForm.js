26a24ad3c5a6a1382d16daafba86085fc0c2aad2
N.Zhilcov <d.chertkov@getproduction.ru>
2024-11-20 14:27:20 +0500

    useState

--- nylon-3d/src/forms/callbackForm.js
+++ nylon-3d/src/forms/callbackForm.js
@@ -1,15 +1,26 @@
 import styles from './callbackForm.module.css';
+import { useState } from 'react';
 
 
 
 function CallbackForm() {
+    const [count, setCount] = useState(0);
+    const increment = () => {
+      setCount((currentValue) => currentValue + 1);
+
+    }
+
+
+
     return (
-      <form className={styles.callbackForm}>
+      <form action="" className={styles.callbackForm}>
         <div style={{color:'#fff'}}>Вы можете написать нам и мы обязательно вам перезвоним!</div>
         <input className={styles.formInput} type="text" placeholder="Ф.И.О"></input>
-        <input className={styles.formInput} type="text" placeholder="Телефон"></input>
-        {/* <input className={styles.formInput} type="file"></input> */}
-        <button className={styles.buttonInput} type='submit'>Связаться с нами</button>
+        <input className={styles.formInput} type="tel" placeholder="Телефон"></input>
+        <input className={styles.formInput} type="file"></input>
+        <div className={styles.buttonInput} onClick={increment}>Количество штук: {count}</div>
+        <button className={styles.buttonInput} type='submit'>Расчитать печать</button>
+        
       </form>
     );
   }
